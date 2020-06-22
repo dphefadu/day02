b26ba962075901421452f7e1d32ad18eb63579ee
Delight Phefadu <dphefadu@c5r9s10.wethinkcode.co.za>
2020-06-22 15:23:43 +0200

    day02

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -1,29 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dphefadu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/19 14:36:14 by dphefadu          #+#    #+#             */
-/*   Updated: 2020/06/22 09:03:02 by dphefadu         ###   ########.fr       */
+/*   Created: 2020/06/22 15:13:22 by dphefadu          #+#    #+#             */
+/*   Updated: 2020/06/22 15:14:20 by dphefadu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<unistd.h>
-void    ft_putchar(char	c)
-	{ 
-	   write(1, &c, 1 );
-	}	   
-void    ft_print_numbers(void)
-{
-    char    digit;
 
-    digit ='0';
-    while ( digit <='9')
-    {
-        ft_putchar(digit);
-        digit++;
-    }
+
+
+#include <unistd.h>
+
+void ft_putchar(char a)
+{
+	write(1, &a, sizeof(char));
+}
+void ft_putnbr(int nb)
+{
+
+	if  (nb >0)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+		if(nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb* -1);
+	}
 }
 
+int main ()
+{	
+	ft_putnbr(10);
+	
+	return 0 ;}
 
